2fcf38765966704f650e6ab2871f86c962c06378
Enrique Alfaro <34046353+EnriqueAlfaro97@users.noreply.github.com>
2026-02-16 21:12:26 -0600

    Push

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://EnriqueAlfaro97.github.io',
+  base: 'Portafolio',
   vite: {
     plugins: [tailwindcss()]
   }
